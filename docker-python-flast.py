50c23d5335dbeb53387f9462175aa09d847e65bf
1204235 <58256388+1204235@users.noreply.github.com>
2021-11-25 14:17:51 +0530

    Updated docker file

--- docker-python-flast.py
+++ docker-python-flast.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 def hello():
 	return "Hello Airbus"
 
+
 if __name__ == "__main__":
-	app.run(host ='18.205.19.222', port = 5001, debug = True)
+	app.run(host ='0.0.0.0', port = 5001, debug = True)
